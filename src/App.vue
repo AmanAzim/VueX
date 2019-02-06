98858dea63f8c4c8ef5fd96867289dd57be11f59
Aman <rumman.djuice1@gmail.com>
2019-02-06 20:04:20 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
+               <!-- <app-result :counter="counter"></app-result> -->
+                <app-result></app-result>
                 <hr>
-                <app-counter @updated="counter += $event"></app-counter>
+               <!-- <app-counter @updated="counter += $event"></app-counter> -->
+                <app-counter></app-counter>
             </div>
         </div>
     </div>
@@ -18,7 +20,7 @@
     export default {
         data() {
             return {
-                counter: 0
+               // counter: 0
             }
         },
         components: {
